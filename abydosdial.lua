166f71902e5bfa339815a96af0a6ff2da5a148a3
Echo <167356448+EchoDidAThing1048@users.noreply.github.com>
2025-03-31 16:51:30 -0400

    Create abydosdial.lua

---
+++ abydosdial.lua
@@ -0,0 +1,99 @@
+local interface = peripheral.find("advanced_crystal_interface") or peripheral.find("crystal_interface") or peripheral.find("basic_interface")
+local symbol1 = 26
+local symbol2 = 6
+local symbol3 = 14
+local symbol4 = 31
+local symbol5 = 11
+local symbol6 = 29
+local symbol7 = 0
+
+print("Dialing Chevron 1...")
+sleep(1)
+
+interface.rotateClockwise(symbol1)
+sleep(5)
+interface.openChevron()
+interface.encodeChevron()
+sleep(1)
+interface.closeChevron()
+
+--locked
+print("Chevron 1 Locked")
+sleep(1)
+print("Dialing Chevron 2...")
+
+interface.rotateClockwise(symbol2)
+sleep(5)
+interface.openChevron()
+interface.encodeChevron()
+sleep(1)
+interface.closeChevron()
+
+--locked
+print("Chevron 2 Locked")
+sleep(1)
+print("Dialing Chevron 3...")
+
+interface.rotateClockwise(symbol3)
+sleep(5)
+interface.openChevron()
+interface.encodeChevron()
+sleep(1)
+interface.closeChevron()
+
+--locked
+print("Chevron 3 Locked")
+sleep(1)
+print("Dialing Chevron 4...")
+
+--Next Paste Starts here
+interface.rotateClockwise(symbol4)
+sleep(5)
+interface.openChevron()
+interface.encodeChevron()
+sleep(1)
+interface.closeChevron()
+
+--locked
+print("Chevron 4 Locked")
+sleep(1)
+print("Dialing Chevron 5...")
+
+--Next Paste Starts here
+interface.rotateClockwise(symbol5)
+sleep(5)
+interface.openChevron()
+interface.encodeChevron()
+sleep(1)
+interface.closeChevron()
+
+--locked
+print("Chevron 5 Locked")
+sleep(1)
+print("Dialing Chevron 6...")
+
+--Next Paste Starts here
+interface.rotateClockwise(symbol6)
+sleep(5)
+interface.openChevron()
+interface.encodeChevron()
+sleep(1)
+interface.closeChevron()
+
+--locked
+print("Chevron 6 Locked")
+sleep(1)
+print("Dialing Final Chevron...")
+
+--Next Paste Starts here
+interface.rotateClockwise(symbol7)
+sleep(5)
+interface.openChevron()
+interface.encodeChevron()
+sleep(1)
+interface.closeChevron()
+
+--locked
+print("Final Chevron Locked!")
+sleep(1)
+print("Wormhole established")
